9050fecc668e9a6ea4fc0ffeb57a54ee71571ae3
Петър Марковски <pm08106567@edu.mon.bg>
2020-10-14 16:29:13 +0300

    Use template

---
+++ Services/OnlineMoneyDonationSystem.Services.Data/SettingsService.cs
@@ -0,0 +1,29 @@
+﻿namespace OnlineMoneyDonationSystem.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using OnlineMoneyDonationSystem.Data.Common.Repositories;
+    using OnlineMoneyDonationSystem.Data.Models;
+    using OnlineMoneyDonationSystem.Services.Mapping;
+
+    public class SettingsService : ISettingsService
+    {
+        private readonly IDeletableEntityRepository<Setting> settingsRepository;
+
+        public SettingsService(IDeletableEntityRepository<Setting> settingsRepository)
+        {
+            this.settingsRepository = settingsRepository;
+        }
+
+        public int GetCount()
+        {
+            return this.settingsRepository.AllAsNoTracking().Count();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            return this.settingsRepository.All().To<T>().ToList();
+        }
+    }
+}
